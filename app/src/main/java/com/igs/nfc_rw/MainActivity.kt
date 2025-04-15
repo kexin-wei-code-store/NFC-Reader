7eff0b9b2908d7a1d3fe03a74b833edf3fea0fdf
Kexin-Wei <vacationwkx@gmail.com>
2025-04-15 17:49:55 +0800

    [add] nfc tag detected

--- app/src/main/java/com/igs/nfc_rw/MainActivity.kt
+++ app/src/main/java/com/igs/nfc_rw/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.igs.nfc_rw
 
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
 import android.nfc.NfcAdapter
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -27,8 +32,13 @@ import com.igs.nfc_rw.utils.Logger
 
 class MainActivity : ComponentActivity() {
     private var nfcAdapter: NfcAdapter? = null
+    private var nfcPendingIntent: PendingIntent? = null
+    private val nfcIntentFiltersArarry: Array<IntentFilter>? = null
+    private val nfcTechListsArray: Array<Array<String>>? = null
+
     private val loggerHead = "MainActivity"
 
+    @SuppressLint("UnsafeIntentLaunch")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -57,13 +67,68 @@ class MainActivity : ComponentActivity() {
         }
         Logger.d(loggerHead, "NFC is enabled on this device")
 
+        // Create PendingIntent with proper flags for Android 15 compatibility
+        val flags = PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+
+        // create pending intent for NFC intent
+        val finalFlags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            flags or PendingIntent.FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT
+        } else {
+            flags
+        }
+        nfcPendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, this.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            finalFlags
+        )
+
+        handleIntent(intent)
     }
 
 
-//    override fun onResume() {
-//        super.onResume()
-//        nfcAdapter?.enableForegroundDispatch(this, null, null, null)
-//    }
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(
+            this,
+            { tag ->
+//                // Handle tag discovery here
+//                val tagId = tag.id.toHexString()
+//                val data = readFromTag(tag)
+//
+//                runOnUiThread {
+//                    // Update UI
+//                    tagInfoTextView.text = "Tag ID: $tagId\nData: $data"
+//                }
+                Logger.d(loggerHead, "Tag discovered")
+            },
+            NfcAdapter.FLAG_READER_NFC_A or
+                    NfcAdapter.FLAG_READER_NFC_B or
+                    NfcAdapter.FLAG_READER_NFC_F or
+                    NfcAdapter.FLAG_READER_NFC_V,
+            null
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val action = intent.action
+        Logger.d(loggerHead, "Action: $action")
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == action) {
+            Logger.d(loggerHead, "Tag discovered")
+            val tag = intent.getStringExtra(NfcAdapter.EXTRA_TAG)
+            tag?.let {
+                // get the tag id
+                val tagId = it
+                // do something with the tag id
+                Logger.d(loggerHead, "Tag ID: $tagId")
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/igs/nfc_rw/data/NFCReader.kt
+++ app/src/main/java/com/igs/nfc_rw/data/NFCReader.kt
@@ -1,4 +1,26 @@
 package com.igs.nfc_rw.data
 
-class NFCReader {
+import android.app.Activity
+import android.app.PendingIntent
+import android.nfc.NfcAdapter
+
+class NFCReader(private val activity: Activity) {
+    private var nfcAdapter: NfcAdapter? = null
+    private var nfcPendingIntent: PendingIntent? = null
+
+    var onTagDiscovered: ((tagId: String, data: String) -> Unit)? = null
+
+    init {
+
+    }
+
+    private fun initNfcAdapter() {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(activity)
+        nfcPendingIntent = PendingIntent.getActivity(
+            activity,
+            0,
+            activity.intent, PendingIntent.FLAG_MUTABLE
+        )
+
+    }
 }
